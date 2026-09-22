930b350746b7732f251d28fb377fdc1447bc6e29
Luciano-HP12 <lucianohuallparimachi04@gmail.com>
2026-09-21 23:40:51 -0500

    feat: initialize Andes Travel WordPress theme

---
+++ app/public/wp-content/themes/andes-travel/index.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Main template file.
+ *
+ * @package AndesTravel
+ */
+
+get_header();
+?>
+
+<main>
+    <h1>Andes Travel</h1>
+    <p>Explora Perú con nosotros.</p>
+</main>
+
+<?php
+get_footer();
